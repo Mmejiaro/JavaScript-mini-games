db42c7aee3922f94cb83cd0f0d5f3c4c17cfd288
Michael Mejia <michaelmejia1171@gmail.com>
2021-08-09 18:26:49 -0400

    finished challenge 4

--- static/js/script.js
+++ static/js/script.js
@@ -113,6 +113,58 @@ function rpsFrontEnd(humanImgChoice, botImgChoice, finalMessage) {
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 }
 
-function buttonColorChange() {
+// Challenge 4: Change the Color of All Buttons
+var all_buttons = document.getElementsByTagName('button');
 
+var copyAllButtons = [];
+
+for (let i = 0; i < all_buttons.length; i++) {
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+
+
+function buttonColorChange(optionSelected) {
+    if (optionSelected.value === 'red') {
+        buttonsRed();
+    }
+    else if (optionSelected.value === 'green') {
+        buttonsGreen();
+    }
+    else if (optionSelected.value === 'reset') {
+        buttonColorReset();
+    }
+    else if (optionSelected.value === 'random') {
+        randomColors();
+    }
+}
+
+function buttonsRed() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonsGreen() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset() {
+    for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+function randomColors() {
+    let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning'];
+
+    for (let i = 0; i < all_buttons.length; i++) {
+        let randomNum = Math.floor(Math.random() * 4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNum]);
+    }
 }
